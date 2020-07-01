c08e629ed57b995deea5851c6caa9f952fb4b6e7
AsmaaAlazmi056 <66563370+AsmaaAlazmi@users.noreply.github.com>
2020-07-01 18:50:09 +0300

    asmaa

---
+++ movie/app/src/main/java/com/example/movie/movie.java
@@ -0,0 +1,54 @@
+package com.example.movie;
+
+public class movie {
+
+    private String mainActor;
+    private String title;
+    private double movieRate ;
+    private int pgRate;
+    private String genre;
+
+    public String getMainActor() {
+        return mainActor;
+    }
+
+    public void setMainActor(String mainActor) {
+        this.mainActor = mainActor;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getMovieRate() {
+        return movieRate;
+    }
+
+    public void setMovieRate(double movieRate) {
+        this.movieRate = movieRate;
+    }
+
+    public int getPgRate() {
+        return pgRate;
+    }
+
+    public void setPgRate(int pgRate) {
+        this.pgRate = pgRate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+
+
+
+}
